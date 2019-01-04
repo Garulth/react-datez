2e75f8d509ddd64e54477cac3f8a87116f8fd1f9
HIEU LE <hieu.lethanh@aipacific.vn>
2019-01-04 09:26:40 +0700

    remove unuse code

--- app/components/reactDatez.js
+++ app/components/reactDatez.js
@@ -513,10 +513,8 @@ class ReactDatez extends Component {
                             </button>
                             { this.props.yearButton ? this.props.yearButton :
                             <button className="rdatez-btn rdatez-btn-year-mobile" onClick={this.changeCalendar}>
-                               {
-                                   this.state.yearJumpOpen ? 'D' : 'Y'
-                                }
-                           </button>}
+                                { this.state.yearJumpOpen ? 'D' : 'Y' }
+                            </button>}
                             {!this.state.disabledToday && <button type="button" className="rdatez-btn rdatez-btn-today" onClick={e => this.jumpToToday(e)}>
                                 <svg
 version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
